b0aa91aa25030886d2d9a7c12ca9d463f1251e00
yuchinnnnn <ongyuchin@gmail.com>
2025-06-17 14:42:17 +0800

    Update homepage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,6 +13,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFFf2ede9),
+    );
   }
 }
